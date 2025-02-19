28cdab3c6ccaa4edd23adf9fb12db0d18cf32caf
DeusIntra <deusintra@yahoo.com>
2025-02-19 10:27:24 +0500

    get all kinds of compose.yml files

--- .config/nvim/lua/base/set_filetype.lua
+++ .config/nvim/lua/base/set_filetype.lua
@@ -1,9 +1,14 @@
 local function set_filetype(pattern, filetype)
-    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-        pattern = pattern,
-        command = "set filetype=" .. filetype,
-    })
+  vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = pattern,
+    command = "set filetype=" .. filetype,
+  })
 end
 
-set_filetype({ "docker-compose*.y*ml*" }, "yaml.docker-compose")
+set_filetype({
+  "docker*.yml*",
+  "docker*.yaml*",
+  "*compose*.yml*",
+  "*compose*.yaml*",
+}, "yaml.docker-compose")
 
