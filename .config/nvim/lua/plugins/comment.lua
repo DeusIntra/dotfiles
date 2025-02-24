8cf8fc3e8099978ffddd98314b2d0868ab5d5c67
DeusIntra <deusintra@yahoo.com>
2025-02-24 23:11:04 +0500

    comment ignore empty lines

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -4,5 +4,6 @@ require('ts_context_commentstring').setup {
 
 require("Comment").setup({
   pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+  ignore = '^$'
 })
 
