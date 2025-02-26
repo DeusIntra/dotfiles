9e35a82a385044a7276c77e7f8928600f1db4ac3
DeusIntra <deusintra@yahoo.com>
2025-02-27 00:38:15 +0500

    eslint for react

--- .config/nvim/lua/plugins/lsp/lsp-list.lua
+++ .config/nvim/lua/plugins/lsp/lsp-list.lua
@@ -1,8 +1,8 @@
 local lsps_by_ft = {
   bashls = { "sh" },
   -- cssls = { "css, scss, less" },
-  eslint = { "javascript", "typescript" },
-  html = { "html" },
+  eslint = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+  -- html = { "html" },
   somesass_ls = { "scss" },
   lua_ls = { "lua" },
   ts_ls = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
