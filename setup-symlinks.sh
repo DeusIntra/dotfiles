8968b181451f10228162431aeb59d0e0b8605ea9
DeusIntra <deusintra@yahoo.com>
2025-03-18 00:46:26 +0500

    fixed issues in setup script

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -12,26 +12,26 @@ dotfiles=(
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-for dotfile in ${dotfiles[@]}; do
-  dir_name=$(dirname $dotfile)
-  if [ ! -d $HOME/$dir_name ]; then
-    mkdir -p $HOME/$dir_name
-    echo created NEW directory $dir_name
+for dotfile in "${dotfiles[@]}"; do
+  dir_name=$(dirname "$dotfile")
+  if [ ! -d "$HOME/$dir_name" ]; then
+    mkdir -p "$HOME/$dir_name"
+    echo created NEW directory "$dir_name"
   fi
-  if [ -L $HOME/$dotfile ]; then
-    rm $HOME/$dotfile
-  elif [ -e $HOME/$dotfile ]; then
+  if [ -L "$HOME/$dotfile" ]; then
+    rm "$HOME/$dotfile"
+  elif [ -e "$HOME/$dotfile" ]; then
     path_name=$(dirname "${dotfile}")
-    echo path: $pathname
-    if [ $path_name = "." ]; then
-      mv --backup --suffix=.bak $HOME/$dotfile $HOME/$dotfile.bak
+    echo path: "$path_name"
+    if [ "$path_name" = "." ]; then
+      mv --backup --suffix=.bak "$HOME/$dotfile" "$HOME/$dotfile.bak"
     else
-      mv --backup --suffix=.bak $HOME/$dotfile $HOME/$path_name.bak
+      mv --backup --suffix=.bak "$HOME/$dotfile" "$HOME/$path_name.bak"
     fi
-    echo created backup for $HOME/$dotfile
+    echo created backup for "$HOME/$dotfile"
   fi
 
-  ln -s $SCRIPT_DIR/$dotfile $HOME/$dotfile
-  echo created symlink for $HOME/$dotfile
+  ln -s "$SCRIPT_DIR/$dotfile" "$HOME/$dotfile"
+  echo created symlink for "$HOME/$dotfile"
 done
 
