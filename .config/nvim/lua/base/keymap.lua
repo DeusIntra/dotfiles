f652a7b2c4b1c460d3d6e6581ea316fc0695fc27
DeusIntra <deusintra@yahoo.com>
2025-04-25 10:27:59 +0500

    better scroll

--- .config/nvim/lua/base/keymap.lua
+++ .config/nvim/lua/base/keymap.lua
@@ -2,6 +2,6 @@ vim.g.mapleader = " "
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>pp", "<CMD>Oil<CR>")
 vim.keymap.set("n", "<leader>po", "<CMD>tabnew<CR><CMD>Oil<CR>")
-vim.keymap.set("n", "<C-D>", "<C-D>zz")
-vim.keymap.set("n", "<C-U>", "<C-U>zz")
+vim.keymap.set("n", "<C-D>", "M<C-D>zz")
+vim.keymap.set("n", "<C-U>", "M<C-U>zz")
 
