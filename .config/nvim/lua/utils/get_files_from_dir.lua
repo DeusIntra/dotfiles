1a0cf4dddff40294c7bf32c6531ef0f64f913b6d
DeusIntra <deusintra@yahoo.com>
2025-10-24 12:08:42 +0500

    added some util functions

---
+++ .config/nvim/lua/utils/get_files_from_dir.lua
@@ -0,0 +1,15 @@
+local map = require "utils.map"
+
+local function strip_ending(file)
+  return file:gsub("%.lua$", "")
+end
+
+local function get_files_from_dir(dir)
+  local path = vim.fn.stdpath('config') .. '/lua/' .. dir
+  return map(
+    vim.fn.readdir(path, [[v:val =~ '\.lua$']]),
+    strip_ending
+  )
+end
+
+return get_files_from_dir
---
+++ .config/nvim/lua/utils/map.lua
@@ -0,0 +1,9 @@
+local function map(tbl, func)
+    local result = {}
+    for k, v in pairs(tbl) do
+        result[k] = func(v)
+    end
+    return result
+end
+
+return map
