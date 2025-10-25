f5b484f22e502c0f956f51bfdc6fb7082112a835
DeusIntra <deusintra@yahoo.com>
2025-10-25 16:05:01 +0500

    cmp config completed

--- .config/nvim/lua/config/plugins/cmp.lua
+++ .config/nvim/lua/config/plugins/cmp.lua
@@ -1,5 +1,6 @@
 local cmp = require('cmp')
 local luasnip = require("luasnip")
+local lspkind = require("lspkind")
 
 local has_words_before = function()
     unpack = unpack or table.unpack
@@ -49,14 +50,28 @@ cmp.setup({
   -- Let's configure the item's appearance
   -- source: https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance
   formatting = {
-      -- Set order from left to right
-      -- kind: single letter indicating the type of completion
-      -- abbr: abbreviation of "word"; when not empty it is used in the menu instead of "word"
-      -- menu: extra text for the popup menu, displayed after "word" or "abbr"
-      fields = { "abbr", "kind", "menu" },
+    -- Set order from left to right
+    -- kind: single letter indicating the type of completion
+    -- abbr: abbreviation of "word"; when not empty it is used in the menu instead of "word"
+    -- menu: extra text for the popup menu, displayed after "word" or "abbr"
+    fields = { "abbr", "kind", "menu" },
 
-      -- customize the appearance of the completion menu
-      format = function(entry, vim_item)
+    -- customize the appearance of the completion menu
+    format = lspkind.cmp_format({
+      mode = 'symbol_text',
+      maxwidth = {
+        -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+        -- can also be a function to dynamically calculate max width such as
+        -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+        menu = 50, -- leading text (labelDetails)
+        abbr = 50, -- actual suggestion item
+      },
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function (entry, vim_item)
         if entry.source.name == 'nvim_lsp' and entry.source.source.client.name == 'marksman' and vim_item.kind == 'Reference' then
           vim_item.word = '<' .. vim_item.word:gsub("%%(%x%x)", function(hex)
             return string.char(tonumber(hex, 16))
@@ -69,7 +84,8 @@ cmp.setup({
             path = "[Path]",
         })[entry.source.name]
         return vim_item
-      end,
+      end
+    })
   },
 
   sources = cmp.config.sources({
@@ -126,3 +142,4 @@ for _, lsp in ipairs(lsp_list) do
 end
 
 vim.lsp.enable(lsp_list)
+
--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -6,10 +6,16 @@ return {
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-cmdline',
-    'L3MON4D3/LuaSnip',
+    {
+      "L3MON4D3/LuaSnip",
+      -- follow latest release.
+      version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+      -- install jsregexp (optional!).
+      build = "make install_jsregexp"
+    },
     'saadparwaiz1/cmp_luasnip',
     {
-      'onsails/lspkind.nvim', opts = require("config.opts.lspkind")
+      'onsails/lspkind.nvim', opts = {}
     }
   }
 }
