40b09b5a25101ac463b00a969646795e47051124
DeusIntra <deusintra@yahoo.com>
2025-10-26 17:57:01 +0500

    lint config

--- .config/nvim/lua/config/plugins/dap.lua
+++ .config/nvim/lua/config/plugins/dap.lua
@@ -1,24 +1,25 @@
 local get_files_from_dir = require("utils.get_files_from_dir")
 local dap_list = get_files_from_dir("config/dap")
 require("mason-nvim-dap").setup({
-  ensure_installed = dap_list
+  ensure_installed = dap_list,
+  automatic_installation = true
 })
 
 local dap = require("dap")
 local dapui = require("dapui")
 dapui.setup()
-require("nvim-dap-virtual-text").setup()
+require("nvim-dap-virtual-text").setup({})
 
-dap.listeners.before.attach.dapui_config = function ()
+dap.listeners.before.attach.dapui_config = function()
   dapui.open()
 end
-dap.listeners.before.launch.dapui_config = function ()
+dap.listeners.before.launch.dapui_config = function()
   dapui.open()
 end
-dap.listeners.before.event_terminated.dapui_config = function ()
+dap.listeners.before.event_terminated.dapui_config = function()
   dapui.close()
 end
-dap.listeners.before.event_exited.dapui_config = function ()
+dap.listeners.before.event_exited.dapui_config = function()
   dapui.close()
 end
 
@@ -27,7 +28,7 @@ vim.fn.sign_define("DapStopped", { text = "➤", texthl = "", linehl = "", numhl
 
 vim.keymap.set('n', '<F4>', dapui.close)
 
---[[ Setup all dap configs ]]--
+--[[ Setup all dap configs ]] --
 local require_all = require("utils.require_all")
 require_all("config/dap")
 
@@ -40,4 +41,3 @@ vim.keymap.set('n', '<leader>B', dap.set_breakpoint)
 vim.keymap.set('n', '<leader>lp', function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
 vim.keymap.set('n', '<leader>dr', dap.repl.open)
 vim.keymap.set('n', '<leader>dl', dap.run_last)
-
--- .config/nvim/lua/config/plugins/ibl-rainbow-delimiters.lua
+++ .config/nvim/lua/config/plugins/ibl-rainbow-delimiters.lua
@@ -7,7 +7,7 @@ local highlight = {
   "RainbowViolet",
   "RainbowCyan",
 }
-local hooks = require "ibl.hooks"
+local hooks = require("ibl.hooks")
 -- create the highlight groups in the highlight setup hook, so they are reset
 -- every time the colorscheme changes
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
